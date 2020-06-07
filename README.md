5d5e8c5f89eb0573f1aa4ac49c6fc8630dabaa81
Asad Saeeduddin <masaeedu@gmail.com>
2020-06-07 01:32:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mapnad
+
+Apparently maps with keys that are monoids are sometimes monads
---
+++ src/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE ViewPatterns #-}
+module Main where
+
+import Data.Map.Strict
+import Data.Proxy
+
+import Control.Monad
+
+import Test.QuickCheck
+import Test.QuickCheck.Classes
+
+newtype Mapnad k v = Mapnad { runMapnad :: Map k v }
+  deriving (Show, Eq, Arbitrary, Functor)
+
+instance (Ord k, Monoid k) => Applicative (Mapnad k)
+  where
+  pure = return
+  (<*>) = ap
+
+joinMapnad :: (Ord k, Monoid k) => Mapnad k (Mapnad k v) -> Mapnad k v
+joinMapnad (Mapnad (fmap runMapnad -> mm)) = Mapnad $ fromList $ fmap (\(k1, (k2, v)) -> (k1 <> k2, v)) $ (>>= sequenceA) $ toList $ fmap toList $ mm
+
+instance (Ord k, Monoid k) => Monad (Mapnad k)
+  where
+  return = Mapnad . singleton mempty
+  ma >>= amb = joinMapnad $ fmap amb ma
+
+main :: IO ()
+main = lawsCheck $ monadLaws $ Proxy @(Mapnad String)
