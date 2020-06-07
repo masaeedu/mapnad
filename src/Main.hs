56be2056f9e1ae97446b7ce821a95293ca563024
Asad Saeeduddin <masaeedu@gmail.com>
2020-06-07 02:04:20 -0400

    Switch to hedgehog-classes, test some more kinds of keys

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,18 @@
 module Main where
 
 import Data.Map.Strict
-import Data.Proxy
+import Data.Monoid
+import Data.Functor
 
 import Control.Monad
 
-import Test.QuickCheck
-import Test.QuickCheck.Classes
+import Hedgehog
+import qualified Hedgehog.Gen as G
+import qualified Hedgehog.Range as R
+import Hedgehog.Classes
 
 newtype Mapnad k v = Mapnad { runMapnad :: Map k v }
-  deriving newtype (Show, Eq, Arbitrary, Functor)
+  deriving newtype (Show, Eq, Functor)
 
 instance (Ord k, Monoid k) => Applicative (Mapnad k)
   where
@@ -25,5 +28,20 @@ instance (Ord k, Monoid k) => Monad (Mapnad k)
   return = Mapnad . singleton mempty
   ma >>= amb = joinMapnad $ fmap amb ma
 
+aGoodSize :: Range Int
+aGoodSize = R.linear 0 10
+
+genMap :: (Ord k, Monoid k) => Gen k -> Gen a -> Gen (Mapnad k a)
+genMap k g = Mapnad <$> G.map aGoodSize ((,) <$> k <*> g)
+
+sumgen :: Gen (Sum Int)
+sumgen = Sum <$> G.int aGoodSize
+
+mulgen :: Gen (Product Int)
+mulgen = Product <$> G.int aGoodSize
+
+strgen :: Gen String
+strgen = G.string aGoodSize G.alpha
+
 main :: IO ()
-main = lawsCheck $ monadLaws $ Proxy @(Mapnad String)
+main = void $ lawsCheck $ monadLaws $ genMap strgen
